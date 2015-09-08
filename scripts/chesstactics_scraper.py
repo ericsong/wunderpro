18faf2733cf1ddcb2728eff6ab1f3c0f48471487
ericsong <regonics@gmail.com>
2015-09-08 14:42:55 -0400

    finished CT scraper, create scraped file

--- scripts/chesstactics_scraper.py
+++ scripts/chesstactics_scraper.py
@@ -10,5 +10,14 @@ data = r.text
 
 soup = BeautifulSoup(data)
 
+read = '1'
+
 for link in soup.select('tr td a.smallgraytext'):
-  print(link.get('href').split('From')[1][1:])
+  section_string = link.get('href').split('From')[1][1:][:-2]
+  parsed = section_string.split(',')
+  chapter, section, lesson = parsed
+
+  if chapter == '2' and section == '1' and lesson == '9':
+    read = '0'
+
+  print(section_string + "," + read)
