2bad6f6a38fc2d3212e8817bd4a0975b08e5bf7d
ericsong <regonics@gmail.com>
2015-09-13 11:30:24 -0400

    add read api method

--- tasks.py
+++ tasks.py
@@ -16,6 +16,11 @@ def push_to_api(push_url, payload, patch = False):
       req.get_method = lambda: 'PATCH'
     return urllib2.urlopen(req).read()
 
+def read_from_api(read_url):
+    headers = { 'X-Access-Token' : oauth['mytoken'], 'X-Client-ID' : oauth['client_id'], 'Content-Type' : 'application/json' }
+    req = urllib2.Request(read_url, {}, headers)
+    return urllib2.urlopen(req).read()
+
 @app.task
 def add(x, y):
     return x + y
