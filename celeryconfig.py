f623d31219e8f0b64d862608af8974edea2c61bc
ericsong <regonics@gmail.com>
2015-09-23 20:37:23 -0400

    add attendance report task

--- celeryconfig.py
+++ celeryconfig.py
@@ -24,6 +24,11 @@ CELERYBEAT_SCHEDULE = {
         'schedule': crontab(minute=0, hour=20, day_of_week='tuesday,sunday'),
         'args': ["post casual harmony rehearsal report"]
     },
+        'add-casualharmony-attendance-task': {
+        'task': 'tasks.addTaskToInbox',
+        'schedule': crontab(minute=0, hour=20, day_of_week='tuesday,sunday'),
+        'args': ["make casual harmony attendance report"]
+    },
         'add-dotfiles-config-task': {
         'task': 'tasks.addTaskToInbox',
         'schedule': crontab(minute=0, hour=5),
