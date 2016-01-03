a7967b6d6b57c8adc3eee61f01601da6e91b0d52
ericsong <regonics@gmail.com>
2016-01-03 11:35:37 +0900

    base page loads tasks with ajax

---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log('hi');
+
+$.get('/tasks', function(data) {
+    console.log(data);
+});
--- server.py
+++ server.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, send_from_directory
 from config_extractor import getParsedTasks
 import json
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+app.debug = True
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
 
 @app.route('/')
 def hello_world():
+    return render_template('index.html')
+
+@app.route('/tasks')
+def getTasks():
     tasks = getParsedTasks()
 
-    return json.dumps(tasks)
+    return jsonify(tasks = tasks)
 
 if __name__ == '__main__':
     app.run()
