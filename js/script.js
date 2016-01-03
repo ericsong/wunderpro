803e2b9b48f4cb9e0ad1fd47fb2538b830bc4f02
ericsong <regonics@gmail.com>
2016-01-03 11:41:39 +0900

    displaying tasks in page (crude)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-console.log('hi');
+function createTaskItem(task) {
+    var text = task.task + ", " + task.title + ", " + task.schedule;
+    return $("<li></li").text(text);
+}
 
 $.get('/tasks', function(data) {
+    var list = $('.taskList')[0];
+
+    for(var i = 0; i < data.tasks.length; i++) {
+        var task = data.tasks[i];
+        $(list).append(createTaskItem(task));
+    }
     console.log(data);
 });
