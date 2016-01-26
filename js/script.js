7a584148cf66cc93cf6748fd105fa5b1844f80d4
ericsong <regonics@gmail.com>
2016-01-26 16:29:48 -0500

    display tasks in text editable boxes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
+function createTaskInputItem(text, name) {
+    return $('<div></div>').append(name + '   <input type="text" name=' + name + ' value=' + text + '>');
+}
+
 function createTaskItem(task) {
     var text = task.task + ", " + task.title + ", " + task.schedule;
-    return $("<li></li").text(text);
+    return $("<li></li>"
+        ).addClass('task-list'
+        ).append($('<div></div>'
+            ).addClass('task-item'
+            ).append(createTaskInputItem(task.title, 'Title')
+            ).append(createTaskInputItem(task.task, 'Type')
+            ).append(createTaskInputItem(task.schedule, 'Schedule')
+            ).append(createTaskInputItem(task.args, 'Args'))
+        );
 }
 
 $.get('/tasks', function(data) {
